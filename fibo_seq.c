3f3d49f9052ab296946400d0868bcf7170d56acb
Grégory Mounié <Gregory.Mounie@imag.fr>
2017-05-22 22:03:55 +0200

    style linux

--- fibo_seq.c
+++ fibo_seq.c
@@ -4,19 +4,19 @@
 #include <assert.h>
 
 uint64_t fibo(uint64_t n) {
-  if (n < 2) {
-    return 1;
-      }
-  uint64_t n1;
-  uint64_t n2;
+	if (n < 2) {
+		return 1;
+	}
+	uint64_t n1;
+	uint64_t n2;
 
-  n1 = fibo(n-1);
-  n2 = fibo(n-2);
+	n1 = fibo(n-1);
+	n2 = fibo(n-2);
 
-  return n1 + n2;
+	return n1 + n2;
 }
 
 int main(int argc, char **argv) {
-  assert(argc == 2);
-  printf("%Ld\n", fibo(atoi(argv[1])));
+	assert(argc == 2);
+	printf("%Ld\n", fibo(atoi(argv[1])));
 }
